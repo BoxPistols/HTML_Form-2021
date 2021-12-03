a44a3a2b1b1c149a4e2a27ec83736d0e7384348a
A_ito <BoxPistols@users.noreply.github.com>
2021-12-04 06:11:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,59 @@
 const x = document.getElementById('input-base');
 const s = document.getElementById('select-base');
 const c = document.getElementById('check-base');
+const r = document.getElementById('radio-base');
 const v = document.querySelector('.view');
 
 /* ===== View Result Value ===== */
+// formElement.addEventListener("submit", (event) => {
+// });
+
 const show = () => {
-  // === CheckBox ===
-  const arr = [];
-  const elems = document.getElementsByName('checkBase');
-  // チェックボックスを全部見に行く
-  for (let i = 0; i < elems.length; i++) {
-    // console.log(elems[i].checked); / console.log(elems[i].value);
-    // チェックの入っているフォームの値だけ配列で取り出す
-    if (elems[i].checked) {
-      elems[i].checked === true;
-      arr.push(elems[i].value);
-      console.log(elems[i].value);
+  this.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // === CheckBox ===
+    const arr = [];
+    const elems = document.getElementsByName('checkBase');
+    // チェックボックスを全部見に行く
+    for (let i = 0; i < elems.length; i++) {
+      // console.log(elems[i].checked); / console.log(elems[i].value);
+      // チェックの入っているフォームの値だけ配列で取り出す
+      if (elems[i].checked) {
+        // elems[i].checked === true;
+        arr.push(elems[i].value);
+        // console.log(elems[i].value);
+      }
     }
-  }
-  /* === Input / Selectの値はそのまま取得表示 === */
-  if (x.value !== '') {
-    v.innerHTML = x.value + ', ' + s.value + ', ' + arr;
-  } else {
-    alert('何か書いて');
-  }
+    // === Radio ===
+    const arrR = [];
+    const elemsR = document.getElementsByName('radioBase');
+    for (let i = 0; i < elemsR.length; i++) {
+      if (elemsR[i].checked) {
+        // elemsR[i].checked === true;
+        console.log(elemsR[i].value);
+        arrR.push(elemsR[i].value);
+      }
+    }
+    /* === Input / Selectの値はそのまま取得表示 === */
+    if (x.value != '') {
+      v.innerHTML =
+        '<ul>' +
+        '<li>' +
+        x.value +
+        '</li>' +
+        '<li>' +
+        s.value +
+        '</li>' +
+        '<li>' +
+        arr.join('/') +
+        '</li>' +
+        '<li>' +
+        arrR +
+        '</li>' +
+        '</ul>';
+    } else {
+      // HTMLのrequiredでも対応
+      alert('入力欄は必須です');
+    }
+  });
 };
