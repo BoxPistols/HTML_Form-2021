50b88706cb3eff26dd7d01379403ad287079537a
BoxPistols <ito.atsu.mail@gmail.com>
2021-12-04 06:03:27 +0900

    2 files modified

--- app.js
+++ app.js
@@ -5,48 +5,55 @@ const r = document.getElementById('radio-base');
 const v = document.querySelector('.view');
 
 /* ===== View Result Value ===== */
+// formElement.addEventListener("submit", (event) => {
+// });
+
 const show = () => {
-  // === CheckBox ===
-  const arr = [];
-  const elems = document.getElementsByName('checkBase');
-  // チェックボックスを全部見に行く
-  for (let i = 0; i < elems.length; i++) {
-    // console.log(elems[i].checked); / console.log(elems[i].value);
-    // チェックの入っているフォームの値だけ配列で取り出す
-    if (elems[i].checked) {
-      // elems[i].checked === true;
-      arr.push(elems[i].value);
-      // console.log(elems[i].value);
+  this.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // === CheckBox ===
+    const arr = [];
+    const elems = document.getElementsByName('checkBase');
+    // チェックボックスを全部見に行く
+    for (let i = 0; i < elems.length; i++) {
+      // console.log(elems[i].checked); / console.log(elems[i].value);
+      // チェックの入っているフォームの値だけ配列で取り出す
+      if (elems[i].checked) {
+        // elems[i].checked === true;
+        arr.push(elems[i].value);
+        // console.log(elems[i].value);
+      }
     }
-  }
-  // === Radio ===
-  const arrR = [];
-  const elemsR = document.getElementsByName('radioBase');
-  for (let i = 0; i < elemsR.length; i++) {
-    if (elemsR[i].checked) {
-      // elemsR[i].checked === true;
-      console.log(elemsR[i].value);
-      arrR.push(elemsR[i].value);
+    // === Radio ===
+    const arrR = [];
+    const elemsR = document.getElementsByName('radioBase');
+    for (let i = 0; i < elemsR.length; i++) {
+      if (elemsR[i].checked) {
+        // elemsR[i].checked === true;
+        console.log(elemsR[i].value);
+        arrR.push(elemsR[i].value);
+      }
     }
-  }
-  /* === Input / Selectの値はそのまま取得表示 === */
-  if (x.value !== '') {
-    v.innerHTML =
-      '<ul>' +
-      '<li>' +
-      x.value +
-      '</li>' +
-      '<li>' +
-      s.value +
-      '</li>' +
-      '<li>' +
-      arr.join('/') +
-      '</li>' +
-      '<li>' +
-      arrR +
-      '</li>' +
-      '</ul>';
-  } else {
-    alert('何か書いて');
-  }
+    /* === Input / Selectの値はそのまま取得表示 === */
+    if (x.value != '') {
+      v.innerHTML =
+        '<ul>' +
+        '<li>' +
+        x.value +
+        '</li>' +
+        '<li>' +
+        s.value +
+        '</li>' +
+        '<li>' +
+        arr.join('/') +
+        '</li>' +
+        '<li>' +
+        arrR +
+        '</li>' +
+        '</ul>';
+    } else {
+      // HTMLのrequiredでも対応
+      alert('何か書いて');
+    }
+  });
 };
